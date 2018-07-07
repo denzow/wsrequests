43bda1a56e75828bcd690e0991c08c5897f2659d
denzow <denzow@gmail.com>
2018-07-07 22:38:48 +0900

    [modify] add comments, and docstring.

--- wsrequests/wsrequests.py
+++ wsrequests/wsrequests.py
@@ -29,44 +29,81 @@ class WsRequests:
 
         self.http_client = self._get_http_client()
         self.ws = None
+        self.latest_ws_url = None
 
     def __getattr__(self, attr_name):
+        """
+        Access for not defined attribute dispatch to requests.
+        :param str attr_name:
+        :return:
+        """
         return getattr(self.http_client, attr_name)
 
     @property
     def is_valid_proxy(self):
+        """
+        :return:
+        :rtype: bool
+        """
         return all([
             self.proxy_url,
             self.proxy_port,
         ])
 
     def connect(self, ws_url):
+        """
+        connect websocket endpoint.
+        :param str ws_url:
+        """
         if self.ws:
             self.ws.close()
+            self.logger.debug('disconnect websocket [{}]'.format(self.latest_ws_url))
+            self.ws = None
+        self.logger.debug('connect websocket [{}]'.format(ws_url))
         self.ws = self._get_ws_client(ws_url)
 
     def send_message(self, message):
+        """
+        send message via websocket to connected websocket server
+        :param message:
+        :return:
+        """
         if not self.ws:
             raise WsRequestsError('Not WebSocket Connected.')
-
+        self.logger.debug('send message [{}]'.format(message))
         self.ws.send(json.dumps(message))
 
     def receive_message(self):
+        """
+        receive message via websocket. if no message, wait for new message.
+        :return:
+        """
         message = self.ws.recv()
+        self.logger.debug('receive message [{}]'.format(message))
         return json.loads(message)
 
     def _get_http_client(self):
+        """
+        get requests session instance
+        :return:
+        """
         client = requests.session()
-
         if self.is_valid_proxy:
             client.trust_env = False
             client.proxies.update({
                 'http': self._get_proxy_url(),
                 'https': self._get_proxy_url(),
             })
+            for proxy in client.proxies:
+                self.logger.debug('use proxy [{}]'.format(proxy))
         return client
 
     def _get_ws_client(self, ws_url):
+        """
+        get websocket client instance
+        :param str ws_url:
+        :return:
+        """
         if self.is_valid_proxy:
             ws = create_connection(
                 ws_url,
@@ -80,6 +117,7 @@ class WsRequests:
                 ws_url,
                 cookie=self._get_session_cookie_string(),
             )
+        self.latest_ws_url = ws_url
         return ws
 
     def _get_proxy_url(self):
@@ -97,5 +135,8 @@ class WsRequests:
             )
 
     def _get_session_cookie_string(self):
+        """
+        requests.cookie translate to string. for websocket client.
+        :return:
+        """
         return ';'.join(['{}={}'.format(k, v) for k, v in self.http_client.cookies.items()])
-
